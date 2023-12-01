58fd85012ba8986c5e21ffc65088f5063d221e8f
Austin Almond <aust.almond@gmail.com>
2023-12-01 16:43:52 -0500

    solution for "Young Physicist"

--- a2oj-under-1300/1-young-physicist/main.cpp
+++ a2oj-under-1300/1-young-physicist/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
-int main() {
+using namespace std;
+
+// https://codeforces.com/problemset/problem/69/A
+int main()
+{
     int nLines;
-    std::cin >> nLines;
-    std::cout << "Hi" << std::endl;
+    cin >> nLines;
+
+    int total_x = 0;
+    int total_y = 0;
+    int total_z = 0;
+
+    for (int i = 0; i < nLines; i++)
+    {
+        int x, y, z;
+        cin >> x;
+        cin >> y;
+        cin >> z;
+
+        total_x += x;
+        total_y += y;
+        total_z += z;
+    }
+
+    if (total_x == 0 && total_y == 0 && total_z == 0) {
+        // Equilibrium
+        cout << "YES" << endl;
+    } else {
+        // Not equilibrium
+        cout << "NO" << endl;
+    }
 }
