ab80ba8e30e257ce66ec25662b7313b791a1cc65
Austin Almond <aust.almond@gmail.com>
2023-12-01 22:17:47 -0500

    solution for "Free Cash"

---
+++ a2oj-under-1300/94-free-cash/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+// https://codeforces.com/problemset/problem/237/A
+int main()
+{
+    // Speed up iostream since we don't use stdio
+    std::ios::sync_with_stdio(false);
+
+    // number of cafe visitors
+    // 1 <= n <= 10^5
+    int n_visitors;
+    cin >> n_visitors;
+
+    // Get the time each visitor will arrive
+    uint16_t registers = 1;
+    map<uint16_t, uint16_t> time_frequencies;
+    for (int i = 0; i < n_visitors; i++)
+    {
+        // Each line has h_i and m_i
+        // 0 <= h_i <= 23
+        int h_i;
+        cin >> h_i;
+
+        // 0 <= m_i <= 59
+        int m_i;
+        cin >> m_i;
+
+        // 0 <= minutes <= 1439
+        uint16_t minutes = 60 * h_i + m_i;
+
+        auto freq = time_frequencies.find(minutes);
+        if (freq == time_frequencies.end())
+        {
+            // First client with this time
+            time_frequencies[minutes] = 1;
+        }
+        else
+        {
+            // Multiple clients have this time
+            uint16_t new_freq = freq->second + 1;
+            if (new_freq > registers)
+            {
+                // This is the most popular time - add a cash register
+                registers = new_freq;
+            }
+            freq->second = new_freq;
+        }
+    }
+
+    // Return the number of cash registers needed to ensure no client ever has to wait
+    cout << registers << endl;
+}
