4f9d24aa4f8b2d3ca0a45372f4effe1397793d8b
Austin Almond <aust.almond@gmail.com>
2023-12-01 21:19:18 -0500

    Fix solution - needed long long int

--- a2oj-under-1300/30-effective-approach/main.cpp
+++ a2oj-under-1300/30-effective-approach/main.cpp
@@ -6,14 +6,16 @@ using namespace std;
 // https://codeforces.com/problemset/problem/227/B
 int main()
 {
-    // number of values
+    // number of elements in the array
+    // 1 <= n <= 10^5
     int n;
     cin >> n;
 
-    // Map of value locations
+    // Map of value locations in the array
     map<int, size_t> locations;
 
-    // array of values
+    // Read in the array of values and store their locations
+    // Array contains n distinct integers, where 1 <= a[i] <= n
     for (size_t i = 0; i < n; i++)
     {
         int a_i;
@@ -21,17 +23,20 @@ int main()
         locations[a_i] = i;
     }
 
-    // number of queries
+    // Number of queries
+    // 1 <= m <= 10^5
+    // Queries can repeat
     int m;
     cin >> m;
 
+    long long int forward_sum = 0;
+    long long int backward_sum = 0;
+
+    // Read in m queries.
     // For each query:
     // Find out how many comparisons are needed for each approach:
     // 1. start with the first element and proceed to the end
     // 2. start with the last element and go to the start
-    int forward_sum = 0;
-    int backward_sum = 0;
-
     for (int i = 0; i < m; i++)
     {
         int needle;
