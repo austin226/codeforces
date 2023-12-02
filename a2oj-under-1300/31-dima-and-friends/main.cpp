ea6220316b02b37cfa5f27fecf3bf918c18b0de5
Austin Almond <aust.almond@gmail.com>
2023-12-01 21:45:56 -0500

    solution for "Dima and Friends"

--- a2oj-under-1300/31-dima-and-friends/main.cpp
+++ a2oj-under-1300/31-dima-and-friends/main.cpp
@@ -5,4 +5,32 @@ using namespace std;
 // https://codeforces.com/problemset/problem/272/A
 int main()
 {
+    // Number of friends: 1..=100
+    int n_friends;
+    cin >> n_friends;
+
+    // n_friends positive integers, not exceeding 5, representing how many fingers each friend will show
+    // friend_fingers can range from 1..=500
+    int friend_fingers = 0;
+    for (int i = 0; i < n_friends; i++)
+    {
+        // Each friend holds up 1..=5 fingers.
+        int friend_i_fingers;
+        cin >> friend_i_fingers;
+        friend_fingers += friend_i_fingers;
+    }
+
+    // Calculate how many ways to show some number of fingers on one hand (1 to 5) so Dima doesn't have to clean the place.
+    // Find every x (1 <= x <= 5) such that
+    // (x + friend_fingers) % (n_friends + 1) != 1
+    int n_ways = 0;
+    for (int x = 1; x <= 5; x++)
+    {
+        if ((x + friend_fingers) % (n_friends + 1) != 1)
+        {
+            n_ways++;
+        }
+    }
+
+    cout << n_ways << endl;
 }
