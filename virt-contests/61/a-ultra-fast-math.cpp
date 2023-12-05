5bf7fc11afc0f0359019fe10a4f0d19f8ba9b52d
Austin Almond <aust.almond@gmail.com>
2023-12-04 21:15:58 -0500

    solve 61a

---
+++ virt-contests/61/a-ultra-fast-math.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/61/problem/A
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        int n1 = (a[i] == '0' ? 0 : 1);
+        int n2 = (b[i] == '0' ? 0 : 1);
+        cout << (n1 ^ n2);
+    }
+
+    cout << endl;
+}
