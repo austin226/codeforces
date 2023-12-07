03f0c2de5f9d3e31e866f7f231431dcfb6d760aa
Austin Almond <aust.almond@gmail.com>
2023-12-06 22:56:38 -0500

    make a network/graph class

--- virt-contests/61/d-eternal-victory.cpp
+++ virt-contests/61/d-eternal-victory.cpp
@@ -4,6 +4,7 @@
 #include <cctype>
 #include <cmath>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <set>
 #include <sstream>
@@ -21,14 +22,44 @@ void remove_trailing(string &input, const char char_to_remove) {
   input.erase(input.find_last_not_of(char_to_remove) + 1, string::npos);
 }
 
+template <typename N, typename W>
+struct Edge {
+  N a;
+  N b;
+  W weight;
+};
+
+/// A weighted, undirected graph.
+template <typename N, typename W, typename S>
+class Network {
+ private:
+  vector<Edge<N, W>> edges;
+
+ public:
+  Network(S size) {}
+
+  void AddEdge(N node_a, N node_b, W weight) {
+    Edge edge = {node_a, node_b, weight};
+  }
+};
+
 int main() {
+  // Similar to traveling salesman problem, but can finish in any city.
+  // We want to visit every city at least once.
   // 1 <= n <= 10^5
   uint16_t n_cities;
   cin >> n_cities;
 
+  Network<uint16_t, uint16_t, uint16_t> g(n_cities);
   for (uint16_t i = 0; i < n_cities - 1; i++) {
     // 1 <= x[i], y[i] <= n, 0 <= w[i] <= 2 × 10^4)
+    // x and y are node indexes.
+    // w is the weight of the edge connecting x and y.
     uint16_t x, y, w;
     cin >> x >> y >> w;
+
+    g.AddEdge(x, y, w);
   }
+
+  // calculate the minimum length to visit every node.
 }
