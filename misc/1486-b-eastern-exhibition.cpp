aff340e0950960aee6e0a2ec17e4bd3b538a917c
Austin Almond <aust.almond@gmail.com>
2023-12-17 22:15:35 -0500

    start 1486-b

---
+++ misc/1486-b-eastern-exhibition.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#define F(i, L, R) for (int i = L; i < R; i++)
+#define STR_CONTAINS(s1, s2) s1.find(s2) != std::string::npos
+
+#define SZ(x) ((int)((x).size()))
+#define SORT_VEC(vec) std::sort(vec.begin(), vec.end())
+#define VEC_MIN(vec) std::min_element(vec.begin(), vec.end())
+#define VEC_MAX(vec) std::max_element(vec.begin(), vec.end())
+#define COPY_VEC(vec1, vec2) \
+  std::copy(vec1.begin(), vec1.end(), vec2.begin(), vec2.end())
+#define pb push_back
+
+#define MIN_MAX(a, b) std::tuple(std::min(a, b), std::max(a, b))
+#define PI 3.1415926535897932384626
+
+#define _N_TYPE_MIN(T) std::numeric_limits<T>::min()
+#define _N_TYPE_MAX(T) std::numeric_limits<T>::max()
+
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+// https://codeforces.com/problemset/problem/1486/B
+int main() {
+  int t;
+  cin >> t;
+
+  F(t_i, 0, t) {
+    int n;
+    cin >> n;
+
+    vi x(n);
+    vi y(n);
+    F(i, 0, n) {
+      cin >> x[i] >> y[i];
+    }
+  }
+}
