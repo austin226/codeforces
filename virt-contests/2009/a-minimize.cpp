1fd39cbef6712e69be3a51977e5028e759411c0b
Austin Almond <aust.almond@gmail.com>
2024-09-19 21:01:05 -0400

    Solve 2009/a minimize

--- virt-contests/2009/a-minimize.cpp
+++ virt-contests/2009/a-minimize.cpp
@@ -1,2 +1,16 @@
 // https://codeforces.com/contest/2009/problem/A
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << b - a << endl;
+    }
+}
